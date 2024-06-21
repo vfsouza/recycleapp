84ecac70fbc5b94af9e5fa7ecb04cb9c45380cc4
vfsouza <vini.f.souza@hotmail.com>
2024-06-20 21:02:16 -0300

    Second Commit

--- recycleapp/views.py
+++ recycleapp/views.py
@@ -1,3 +1,4 @@
+# recycleapp/views.py
 import base64
 import io
 import os.path
@@ -8,7 +9,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from ultralytics import YOLO
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from django.http import JsonResponse
 from .serializers import ImageSerializer
 from recycle import settings
@@ -40,5 +41,23 @@ class ImageUploadAndProcessView(APIView):
                     'classes': classes
                 })
 
-            return JsonResponse({'results': processed_results}, status=status.HTTP_200_OK)
+                # Desenhar as bounding boxes na imagem
+                draw = ImageDraw.Draw(processed_image)
+                font = ImageFont.load_default()
+                for box, confidence, cls in zip(boxes, confidences, classes):
+                    draw.rectangle(box, outline="red", width=2)
+                    text = f"{cls}: {confidence:.2f}"
+                    text_size = draw.textbbox((0, 0), text, font=font)  # Obtém a bounding box do texto
+                    text_width = text_size[2] - text_size[0]
+                    text_height = text_size[3] - text_size[1]
+                    text_location = [box[0], box[1] - text_height] if box[1] - text_height > 0 else [box[0], box[1] + text_height]
+                    draw.rectangle([text_location, [text_location[0] + text_width, text_location[1] + text_height]], fill="red")
+                    draw.text(text_location, text, fill="white", font=font)
+
+            # Convert processed image to base64
+            buffered = io.BytesIO()
+            processed_image.save(buffered, format="PNG")
+            processed_img_str = base64.b64encode(buffered.getvalue()).decode()
+
+            return JsonResponse({'processed_img_data': processed_img_str, 'results': processed_results}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
